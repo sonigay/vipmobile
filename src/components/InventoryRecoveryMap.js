4335c9610f04b21a45efb347d9e2ccc240ed9add
sonigay2 <s01058338833@gmail.com>
2025-09-04 02:48:18 +0900

    ESLint 오류 수정: useRef import 추가

--- src/components/InventoryRecoveryMap.js
+++ src/components/InventoryRecoveryMap.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useRef } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Circle, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
