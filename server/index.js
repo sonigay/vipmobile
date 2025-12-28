2e0dc5e7135afe0bda869f06783384f2af83e1e5
sonigay <sonigay@naver.com>
2025-12-28 14:05:01 +0900

    fix: server/index.js 전역 CORS 설정에 x-user-id, x-user-role 헤더 추가

--- server/index.js
+++ server/index.js
@@ -172,7 +172,7 @@ app.options('*', (req, res) => {
   }
 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Origin, Accept, X-API-Key');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Origin, Accept, X-API-Key, x-user-id, x-user-role');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Max-Age', '86400'); // 24시간 캐시
   res.status(200).end();
