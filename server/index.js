0062d2c0daa03e5133d0ae0e3a39e638a20a5e21
sonigay <sonigay@naver.com>
2026-01-04 13:35:01 +0900

    fix: 전역 CORS 설정에 x-mode 헤더 추가
    - server/index.js의 전역 OPTIONS 핸들러에 x-mode 헤더 추가
    - 확인이력 기록 API의 CORS preflight 요청 처리 개선

--- server/index.js
+++ server/index.js
@@ -180,7 +180,7 @@ app.options('*', (req, res) => {
   }
 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Origin, Accept, X-API-Key, x-user-id, x-user-role, x-user-name');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Origin, Accept, X-API-Key, x-user-id, x-user-role, x-user-name, x-mode');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Max-Age', '86400'); // 24시간 캐시
   res.status(200).end();
