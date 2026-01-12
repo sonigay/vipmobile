178e7c1911d092b2217b33b97328a3a7c2044922
sonigay <sonigay@naver.com>
2026-01-13 05:35:53 +0900

    개통정보입력페이지: useCallback import 추가

--- src/components/direct/OpeningInfoPage.js
+++ src/components/direct/OpeningInfoPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo, useRef } from 'react';
+import React, { useState, useEffect, useMemo, useRef, useCallback } from 'react';
 import {
     Box,
     Paper,
